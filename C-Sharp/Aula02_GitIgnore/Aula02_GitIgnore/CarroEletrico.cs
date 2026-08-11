640b6af7b15c4d5a609a6b81370823c855df4de3
Victor Ferreira <vitinho.belo005@icloud.com>
2026-08-10 21:22:21 -0300

    Add files via upload

---
+++ C-Sharp/Aula02_GitIgnore/Aula02_GitIgnore/CarroEletrico.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula02_GitIgnore
+{
+    internal class CarroEletrico
+    {
+        // pega e modifica a informação - encapsulamento
+        // Declarações
+
+        public string Modelo { get; set; } = string.Empty;
+
+        public double ValorBase {  get; set;} 
+
+        public int Autonomia { get; set;} 
+
+        public int Idade { get; set; }
+
+
+        // Função 
+        public double CalcularDesconto()
+        {
+            const double Desconto = 0.10;
+            return ValorBase * Desconto;
+        }
+        public double CalcularValorDesconto()
+        {
+            return ValorBase - CalcularDesconto();
+        }
+
+        public double CalcularParcela() 
+        {
+            const double Juros_Financiamento = 5000.00;
+            double valorTotalFinanciamento = CalcularValorDesconto() + Juros_Financiamento;
+            return valorTotalFinanciamento/36;
+        }
+    }
+}
