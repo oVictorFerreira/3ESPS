ee67c801116a1120566683e4c0ef9918bbf831a2
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-17 20:33:31 -0300

    chore: estrutura inicial do projeto bike + features

---
+++ Aula02_GitIgnore/Aula02_GitIgnore/Program.cs
@@ -0,0 +1,11 @@
+﻿using Aula02_GitIgnore;
+
+Console.WriteLine("Sistema de Vendas de Carros Elétricos");
+Console.WriteLine();
+
+// Polimorfismo - Ao invés de chamar CarroEletrico eu chamo só de carro
+// Instância 
+CarroEletrico carro = new CarroEletrico();
+Console.WriteLine("Digite o modelo do carro: ");
+carro.Modelo = Console.ReadLine();
+Console.WriteLine($"Modelo: {carro.Modelo.Trim().ToUpper()}");
---
+++ Aula03_ProjetoLojaBike/Aula03_ProjetoLojaBike/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Aula03_LojaBike;
+
+
+Console.WriteLine("*************** SISTEMA DA LOJA BIKE CITY ***************");
+Console.WriteLine();
+
+// Instanciando objeto da classe Bike
+
+Bike bike = new Bike();
+
+Console.Write("Modelo Bike: ");
+bike.Modelo = Console.ReadLine();
+
+Console.Write("Capacidade da Bateria: ");
+//Precisamos converter pra double
+bike.Bateria = double.Parse(Console.ReadLine());
+
+Console.Write("Peso do Ciclista: ");
+bike.PesoCiclista = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Quantidade Estimada de recargas por mês: ");
+bike.RecargaMes = int.Parse(Console.ReadLine());
+
+
+//Exibit na tela o resultado
+Console.WriteLine("\n------ RELATÓRIO DE DESEMPENHO DA BIKE ------");
+Console.WriteLine($"Bicicleta: {bike.Modelo.ToUpper()}");
+Console.WriteLine($"Autonomia Estimada: {bike.CalcularAutonomia():F1}KM por carga");
+Console.WriteLine($"Consumo mensal: {bike.CalcularConsumoMensal()}KWh");
+Console.WriteLine($"Custo Mensal: {bike.CalcularCustoMensal():N2}");
+Console.WriteLine($"A Bike é econômica {bike.ModeloEconomico()}");
+
