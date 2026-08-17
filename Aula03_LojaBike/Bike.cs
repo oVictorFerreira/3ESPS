ab40443208afac8f6c3694f299596dc008523fa1
Victor Ferreira <joao.ferreira362@outlook.com>
2026-08-17 20:08:59 -0300

    feat: criando as funções da classe

--- Aula03_LojaBike/Bike.cs
+++ Aula03_LojaBike/Bike.cs
@@ -8,5 +8,42 @@ namespace Aula03_LojaBike
 {
     internal class Bike
     {
+
+        //Declarações
+        public string Modelo { get; set; }
+
+        public double Bateria {  get; set; }
+
+        public double PesoCiclista {  get; set; }
+
+        public int RecargaMes {  get; set; }
+
+        //Métodos
+        public double CalcularAutonomia()
+        {
+            double autonomiaBase = Bateria / 15;
+            Boolean pesoElevado = PesoCiclista > 90.0;
+
+            //Operador ternário
+            return pesoElevado ? (autonomiaBase * 0.05) : (autonomiaBase);
+
+        }
+
+        public double CalcularConsumoMensal()
+        {
+            double kmPorCarga = Bateria / 1000.0;
+            return kmPorCarga * RecargaMes;
+        }
+
+        public double CalcularCustoMensal()
+        {
+            const double PRECO_KWH = 0.80;
+            return CalcularConsumoMensal() * PRECO_KWH;
+        }
+
+        public bool ModeloEconomico()
+        {
+            return (CalcularCustoMensal() < 15.00 || (CalcularAutonomia() > 40.0));
+        }
     }
 }
