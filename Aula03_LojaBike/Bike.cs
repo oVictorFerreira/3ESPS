987cfd1e14629693afa2a68a6e765cf0aab45150
Victor Ferreira <joao.ferreira362@outlook.com>
2026-08-17 19:56:32 -0300

    chore: estrutura inicial do projeto bike

---
+++ Aula03_LojaBike/Bike.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula03_LojaBike
+{
+    internal class Bike
+    {
+    }
+}
